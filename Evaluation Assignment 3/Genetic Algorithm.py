306fe49ea0b0b709be006c85e76a6bd4561adb13
George Rahul <75750164+georgerahul24@users.noreply.github.com>
2024-03-20 10:08:19 +0530

    Handles Negative Numbers also GA

--- Evaluation Assignment 3/Genetic Algorithm.py
+++ Evaluation Assignment 3/Genetic Algorithm.py
@@ -7,7 +7,7 @@ MutationProbability = 0.2
 
 class Chromosome:
     name: str
-    genes: list
+    genes: list  # [negative/positive,bit,bit,bit]
     depth: int
 
     def __init__(self, name: str, genes: list, depth: int):
@@ -21,9 +21,11 @@ class Chromosome:
     def geneValue(self):
         val = 0
         n = 1
-        for gene in self.genes[::-1]:
+        for gene in self.genes[-1:-4:-1]:
             val += gene * n
             n *= 2
+        if self.genes[0] == 1:
+            return -val
         return val
 
     def f(self, x):
@@ -44,8 +46,8 @@ def crossover(chromosomes: list[Chromosome]):
         genes1 = chromosome1.genes
         genes2 = chromosome2.genes
 
-        newgenes1 = [genes1[0], genes1[1], genes2[2]]
-        newgenes2 = [genes2[0], genes2[1], genes1[2]]
+        newgenes1 = [genes1[0], genes1[1], genes2[2], genes2[3]]
+        newgenes2 = [genes2[0], genes2[1], genes1[2], genes1[3]]
 
         newChromosomes.append(
             Chromosome(f"{chromosome1.name}a {chromosome2.name}b", newgenes1, chromosome1.depth + 1))
@@ -62,27 +64,27 @@ def mutation(chromosomes: list[Chromosome]):
     for chromosome in chromosome_copy:
         genes = chromosome.genes
         if random.random() < MutationProbability:
-            new_chromosome = [random.randint(0, 1), genes[1], genes[2]]
+            new_chromosome = [random.randint(0, 1), genes[1], genes[2], genes[3]]
             newChromosomes.append(Chromosome(chromosome.name + "m1", new_chromosome, chromosome.depth))
         if random.random() < MutationProbability:
-            new_chromosome = [genes[0], random.randint(0, 1), genes[2]]
+            new_chromosome = [genes[0], random.randint(0, 1), genes[2], genes[3]]
             newChromosomes.append(Chromosome(chromosome.name + "m2", new_chromosome, chromosome.depth))
         if random.random() < MutationProbability:
-            new_chromosome = [genes[0], genes[1], random.randint(0, 1), ]
+            new_chromosome = [genes[0], genes[1], random.randint(0, 1), genes[3]]
             newChromosomes.append(Chromosome(chromosome.name + "m3", new_chromosome, chromosome.depth))
+            if random.random() < MutationProbability:
+                new_chromosome = [genes[0], genes[1], genes[2], random.randint(0, 1), ]
+                newChromosomes.append(Chromosome(chromosome.name + "m3", new_chromosome, chromosome.depth))
     return newChromosomes + chromosomes
 
 
 Population: list[Chromosome] = []
 for i in range(PopulationSize):
-    chromosome = Chromosome(f"i{i}", [random.randint(0, 1) for i in range(3)], 0)
+    chromosome = Chromosome(f"i{i}", [random.randint(0, 1) for i in range(4)], 0)
     Population.append(chromosome)
 
 print("Initial Population Size:", PopulationSize)
-print("Initial Population:", end=" ")
-for chromosome in Population:
-    print(chromosome, end=", ")
-print()
+
 maxNumberOfGeneration = 1
 currentGeneration = 0
 
@@ -94,6 +96,8 @@ while currentGeneration < maxNumberOfGeneration:
     print("Generation: ", currentGeneration, ", Min Fitness :", sortedPopulation[0].fitness(), " for Chromosome ",
           sortedPopulation[0])
     Population = sortedPopulation[:PopulationSize]
-    if (sortedPopulation[0].fitness() == 0):
+    if sortedPopulation[0].fitness() == 0:
         break
     currentGeneration += 1
+
+print("Number of Generations taken:", currentGeneration)
